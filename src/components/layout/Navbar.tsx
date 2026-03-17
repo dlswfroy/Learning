2dc4ff46b6ea20c38f4d1b6657d7bb6702539791
Dhananjoy Kumar Roy <dlswf.roy@gmail.com>
2026-03-17 01:01:00 +0000

    কালো শেভ দাও

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -56,10 +56,10 @@ export function Navbar() {
           )}
         </div>
         <div className="flex flex-col">
-          <h1 className="text-xl md:text-4xl font-black font-headline tracking-tighter drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] leading-tight text-white uppercase scale-y-110 origin-left">
+          <h1 className="text-xl md:text-4xl font-black font-headline tracking-tighter drop-shadow-[0_3px_3px_rgba(0,0,0,1)] leading-tight text-white uppercase scale-y-110 origin-left">
             {appName}
           </h1>
-          <p className="text-[10px] md:text-xs font-black text-yellow-300 italic leading-none mt-0.5 drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
+          <p className="text-[10px] md:text-xs font-black text-yellow-300 italic leading-none mt-0.5 drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]">
             Smart learning, Bright Future.
           </p>
         </div>
